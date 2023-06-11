f81114c7d08bc903bc835210d220fecebadd3b0b
Dmytro Shubchynskyi <shubchinskiydmitriy@gmail.com>
2023-06-11 02:25:51 +0200

    add timeout into Dockerfile work correct first launch from hub. add liquibase

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
 FROM openjdk:17-jdk-alpine
 MAINTAINER d.shubchynskyi@gmail.com
+
+ENV DOCKERIZE_VERSION v0.6.1
+RUN wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+    && tar -C /usr/local/bin -xzvf dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+    && rm dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz
+
 COPY target/TodoList-0.0.1-SNAPSHOT.jar TodoList-0.0.1-SNAPSHOT.jar
-ENTRYPOINT ["java","-jar","TodoList-0.0.1-SNAPSHOT.jar"]
+
+ENTRYPOINT ["dockerize", "-wait", "tcp://mysql:3306", "-timeout", "60s", "java", "-jar", "/TodoList-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/example/todolist/TodoListApplication.java
+++ src/main/java/com/example/todolist/TodoListApplication.java
@@ -1,8 +1,5 @@
 package com.example.todolist;
 
-import com.example.todolist.entity.Status;
-import com.example.todolist.entity.Task;
-import com.example.todolist.service.TaskService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,26 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoListApplication {
 
-
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
         var context = SpringApplication.run(TodoListApplication.class, args);
 
-        TaskService taskService = context.getBean(TaskService.class);
-
-        Task task = Task.builder()
-                .description("New task number 1")
-                .status(Status.IN_PROGRESS)
-                .build();
-
-        System.out.println(task);
-
-        Task savedTask = taskService.createOrUpdateTask(task);
-        System.out.println(savedTask);
-
-        taskService.deleteTask(task);
-
-        taskService.findAll().forEach(System.out::println);
-
     }
-
 }
